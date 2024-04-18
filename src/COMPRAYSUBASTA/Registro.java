9de5533f71bc74d1403e1549b3aec1177a4ede79
dj.escobar <141870156+DilanJJJ@users.noreply.github.com>
2024-04-17 20:59:47 -0500

    momomom

--- src/COMPRAYSUBASTA/Registro.java
+++ src/COMPRAYSUBASTA/Registro.java
@@ -1,5 +1,27 @@
 package COMPRAYSUBASTA;
 
-public class Registro {
+import PIEZAS.PiezaSubasta;
+import USUARIOS.Comprador;
 
+public class Registro {
+	
+	private PiezaSubasta pieza;
+	private Comprador comprador;
+	
+	
+	
+	public Registro(PiezaSubasta pieza, Comprador comprador) {
+		this.pieza = pieza;
+		this.comprador = comprador;
+	}
+
+	public PiezaSubasta getPieza() {
+		return pieza;
+	}
+	
+	public Comprador getComprador() {
+		return comprador;
+	}
+
+	
 }
